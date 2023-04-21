fb732864b55d11ea91163a79f7124854e0988556
Nguyen Ha Khue <khuecuxi3@gmail.com>
2023-04-21 11:22:29 +0700

    Deployment 21.4
    - Comment Dto validation

--- src/main/java/fpt/sep490/entity/Comment.java
+++ src/main/java/fpt/sep490/entity/Comment.java
@@ -20,8 +20,10 @@ public class Comment {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
 
+    @Column(name = "content", nullable = false)
     private String content;
 
+    @Column(name = "rating", nullable = false)
     private float rating;
 
     @ManyToOne
--- src/main/java/fpt/sep490/payload/CommentDto.java
+++ src/main/java/fpt/sep490/payload/CommentDto.java
@@ -1,10 +1,25 @@
 package fpt.sep490.payload;
 
 import lombok.Data;
+import org.hibernate.validator.constraints.Length;
+
+import javax.validation.constraints.DecimalMax;
+import javax.validation.constraints.DecimalMin;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
 
 @Data
 public class CommentDto {
+
+    @NotEmpty(message = "Content must not be empty")
+    @Size(min = 10, message = "Content should have at least 10 characters")
     private String content;
+
+    @NotEmpty(message = "Rating must not be empty")
+    @DecimalMin(value = "0", inclusive = false, message = "Rating must greater than 0 and smaller than 5")
     private float rating;
+
+    @NotEmpty(message = "User id must not be empty")
+    @DecimalMax(value = "5", inclusive = false, message = "Rating must greater than 0 and smaller than 5")
     private Long userId;
 }
